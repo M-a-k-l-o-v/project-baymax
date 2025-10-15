d160755759098ca32f590656c08da2c8119e1204
ronin3265 <ahmad.karaja777@gmail.com>
2025-10-15 21:20:43 +0100

    beginning responses lama

--- Reasoning.py
+++ Reasoning.py
@@ -1,5 +1,13 @@
-import ollama
+import ollama,json
 from API_and_tools import *
 MODEL = "llama"
 
+def lam_reasoning(prompt):
+    response = ollama.chat(
+        model = MODEL,
+        messages=[
+            {"role": "user","content": prompt}
+        ]
+    )
+    print(response['message']['content'])
 
